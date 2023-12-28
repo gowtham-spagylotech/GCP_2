c310d5251b688c56873c629275d9205cd8662e69
Harish <harishtrainee235@gmail.com>
2023-12-28 15:03:49 +0530

    About us accordion completed.

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,63 +1,84 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var acc = document.getElementsByClassName("accordion");
+  var acc = document.querySelectorAll(".accordion");
 
-  for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
+
+  acc.forEach(function (accordion) {
+    accordion.addEventListener("click", function () {
       var panel = this.nextElementSibling;
+      if (panel) {
+        // Close all panels
+        acc.forEach(function (otherAccordion) {
+          var otherPanel = otherAccordion.nextElementSibling;
 
-      // Close all panels
-      for (var k = 0; k < acc.length; k++) {
-        var otherPanel = acc[k].nextElementSibling;
+          if (otherPanel && otherPanel !== panel) {
+            otherPanel.style.maxHeight = null;
+            resetAccordionStyles(otherAccordion);
+          }
+        });
 
-        if (otherPanel && otherPanel !== panel) {
-          otherPanel.style.maxHeight = null;
+        // Toggle the "active" class on the clicked element
+        this.classList.toggle("active");
+        // panel.classList.toggle("activePanel")
+        // var bgImgAccordion = accordion.closest('.bg-img-accordion');
+        // setAccordionStyles(accordion);
+        // bgImgAccordion.classList.toggle("activeBg")
 
-          // Reset background image and color when collapsing
-          var otherBgImgAccordion = otherPanel.closest('.bg-img-accordion');
-          otherBgImgAccordion.style.background = "none";
-          otherBgImgAccordion.style.backgroundColor = "#fff";
+        // Remove the "active" class from all other accordion elements
+        acc.forEach(function (otherAccordion) {
+          if (otherAccordion !== accordion) {
+            otherAccordion.classList.remove("active");
+          }
+        });
 
-          // Reset text color to #000 when collapsing
-          var otherTitleSpace = acc[k].querySelector('.accordion-title-space');
-          otherTitleSpace.style.color = "#000";
+        // Toggle panel visibility
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+          resetAccordionStyles(accordion);
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+          setAccordionStyles(accordion);
         }
-      }
-
-      // Toggle the "active" class on the clicked element
-      this.classList.toggle("active");
-
-      // Remove the "active" class from all other accordion elements
-      for (var j = 0; j < acc.length; j++) {
-        if (acc[j] !== this) {
-          acc[j].classList.remove("active");
-        }
-      }
-
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-
-        // Reset background image and color when collapsing
-        var bgImgAccordion = panel.closest('.bg-img-accordion');
-        bgImgAccordion.style.background = "none";
-        bgImgAccordion.style.backgroundColor = "#fff";
-
-        // Reset text color to #000 when collapsing
-        var titleSpace = this.querySelector('.accordion-title-space');
-        titleSpace.style.color = "#000";
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-
-        // Set background image for the clicked panel with height 581px
-        var bgImgAccordion = panel.closest('.bg-img-accordion');
-        var imagePath = "assets/img/Accordion-Bg.jpg";
-        bgImgAccordion.style.background = "url(" + imagePath + ") no-repeat";
-        bgImgAccordion.style.backgroundSize = "cover"; // Add this line
-        bgImgAccordion.style.backgroundColor = "transparent";
-
-        // Set text color to #fff when expanding
-        var titleSpace = this.querySelector('.accordion-title-space');
-        titleSpace.style.color = "#fff";
       }
     });
-  }
+  });
 });
+
+// Function to reset styles for an accordion
+function resetAccordionStyles(accordion) {
+  var bgImgAccordion = accordion.closest('.bg-img-accordion');
+  bgImgAccordion.classList.remove("activeBg");
+
+  var titleSpace = accordion.querySelector('.accordion-title-space');
+  titleSpace.style.color = "#000";
+
+  var plusIcon = accordion.querySelector('.plus-icon');
+  var minusIcon = accordion.querySelector('.minus-icon');
+  plusIcon.style.display = "block";
+  minusIcon.style.display = "none";
+
+  var outerlineimage = document.querySelector('.accordion-outerline-img');
+  if (outerlineimage) {
+    outerlineimage.style.display = "block";
+  }
+
+}
+
+// Function to set styles for an accordion
+function setAccordionStyles(accordion) {
+  var bgImgAccordion = accordion.closest('.bg-img-accordion');
+  bgImgAccordion.classList.add("activeBg");
+
+  var titleSpace = accordion.querySelector('.accordion-title-space');
+  titleSpace.style.color = "#fff";
+
+  var plusIcon = accordion.querySelector('.plus-icon');
+  var minusIcon = accordion.querySelector('.minus-icon');
+  plusIcon.style.display = "none";
+  minusIcon.style.display = "block";
+
+  var outerlineimage = document.querySelector('.accordion-outerline-img');
+  if (outerlineimage) {
+    outerlineimage.style.display = "none";
+  }
+
+}
